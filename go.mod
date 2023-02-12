f5d354d7922162ff4c5e74db5f2476169d35a4a0
Roy Cardich <cardich.nicolas@gmail.com>
2023-02-11 23:51:54 -0500

    Versionamiento

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/nicolascardich/practicando-paquetes-go
+
+go 1.20
+
+require (
+	golang.org/x/text v0.7.0 // indirect
+	rsc.io/quote/v3 v3.1.0
+	rsc.io/sampler v1.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/nicolascardich/practicando-paquetes-go/greet"
+
+	"rsc.io/quote/v3"
+)
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello")             // Hello
+	fmt.Println(greet.Italian())     // Ciao Loki!
+	fmt.Println(quote.HelloV3())     // Hello, world.
+	fmt.Println(quote.Concurrency()) // Concurrency is not parallelism.
 }
